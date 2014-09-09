ec0bf4950bf185206a8dfac84289f5f853972ad4
Adam-Howe <40769@longroad.ac.uk>
2014-09-09 11:02:04 +0100

    Second AS computing program

---
+++ Second Program.py
@@ -0,0 +1,8 @@
+#Adam Howe
+#09/09/14
+#Second Program
+
+#This is a variable that will store the data entered by the user
+first_name = input("Enter your first name")
+#This outputs the name
+print("Hello {0}!".format(first_name))
